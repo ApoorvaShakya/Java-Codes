c6b29ecb5a525504ed7596ec6321241e3741787f
ApoorvaShakya <97945190+ApoorvaShakya@users.noreply.github.com>
2022-02-13 16:44:21 +0530

    Add files via upload

---
+++ myFinal/callMyFinal.java
@@ -0,0 +1,15 @@
+package myFinal;
+
+
+public class callMyFinal
+{
+	MyFinal mf = new MyFinal();
+
+	int sum = mf.addition(44,88);
+	int var1;
+	int var2 = 456;
+	public void myAddition()
+	{
+		mf.addition(33,55);
+	}
+}
